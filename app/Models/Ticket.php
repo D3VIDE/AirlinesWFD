c3b67d086f3c97242349f19ef2875e94a2659a43
D3VIDE <errandojose03@gmail.com>
2025-04-10 13:12:02 +0700

    Temp

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Ticket extends Model
 {
+    //use HasFactory;
     protected $fillable = ['flight_id', 'passenger_name', 'passenger_phone', 'seat_number', 'is_boarding', 'boarding_time'];
     
     //Ini many to one (1/lebih ticket memiliki 1 penerbangan)
