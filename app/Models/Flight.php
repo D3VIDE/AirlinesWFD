7e52263be0a981eb45a2e2cf9b072f4f00f63008
D3VIDE <errandojose03@gmail.com>
2025-04-10 12:43:07 +0700

    First Commit

---
+++ app/Models/Flight.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Flight extends Model
+{
+    use HasFactory;
+    protected $fillable = ['flight_code', 'origin', 'destination', 'departure_time', 'arrival_time'];
+    //ini one to many (1 ticket punya banyak penerbangan)
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+}
+
